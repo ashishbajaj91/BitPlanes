024a499b1cebedb18c849c18d41b6d465394ee75
Shun Yao <shunyaorad@gmail.com>
2017-04-16 15:22:44 -0400

    warpA.h is done. test.h is not

--- Modules/WarpImage/main.cpp
+++ Modules/WarpImage/main.cpp
@@ -1,47 +1,15 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include<opencv2/opencv.hpp>
+#include <cmath>
+#include "warpA.h"
 
-using namespace std;
-using namespace Eigen;
-using namespace cv;
-
-double dNaN = std::numeric_limits<double>::quiet_NaN();
-
-template <class T>
-T Interpolate(cv::Mat &image, float y, float x)
-{
-    float xd, yd;
-    float k1 = modff(x, &xd); // k1 is the fractional, xd is the integer of x
-    float k2 = modff(y, &yd);
-    int xi = int(xd);
-    int yi = int(yd);
-    // Check whether the pixels are within the image
-    if(xi >= 0 && xi < image.cols && yi >=0 && yi < image.rows)
-    {
-        int f1 = xi < image.rows - 1; // Check that pixels to the right
-        int f2 = yi < image.cols - 1; // and to down direction exist.
-        T px1 = image.at<T>(yi, xi);
-        T px2 = image.at<T>(yi, xi + 1);
-        T px3 = image.at<T>(yi + 1, xi);
-        T px4 = image.at<T>(yi + 1, xi + 1);
-        T interpolated_value = (1.0 - k1) * (1.0 - k2) * px1 +
-                               (f1 ? (k1 * (1.0 - k2) * px2) : 0) +
-                               (f2 ? ((1.0 - k1) * k2 * px3) : 0) +
-                               ((f1 && f2) ? (k1 * k2 * px4) : 0);
-        return interpolated_value;
-    }
-    else
-    {
-        return dNaN; // if pixel is not within the image
-    }
-}
 
 int main(int argc, char** argv) {
-    namedWindow("original image");
-    namedWindow("warped image");
+    cv::namedWindow("original image");
+    cv::namedWindow("warped image");
 
-    string filename;
+    std::string filename;
     if (argc > 2)
     {
         filename = argv[1];
@@ -49,43 +17,39 @@ int main(int argc, char** argv) {
     else filename = "/Users/shunyao/cv_imgs/lena_color.jpeg";
 
     // Load image
-    Mat img = imread(filename,0);;
+    cv::Mat img = cv::imread(filename,0);
     cv::Mat img_src_f; img.convertTo(img_src_f, CV_32FC1);
     img_src_f /= 255; // rescale to float
 
-    // Set target image size and type
-    int targetRows = 225;
-    int targetCols = 225;
-    cv::Mat target = Mat(targetRows, targetCols, img_src_f.type(), cv::Scalar(dNaN));
 
-    Matrix3f W(3,3); // warp function
-    W <<    1-0.707,  0.707, 0,
-            0.707,  1-0.707,  0,
-            0,  0,  1;
-    W = W.inverse().eval();
-    cout << W << endl;
-    Vector3f X;      // Point in coordinate frame of source.
-    Vector3f U;      // Point in coordinate frame of target.
+//    cv::Mat img_src_f = (cv::Mat_<float>(4,5) <<
+//            0.3000, 0.6000, 0.9000, 0.1000, 0.5000,
+//            0.1000, 0.2000, 0.3000, 0.4000, 0.5000,
+//            0.3000, 0.9000, 0.8000, 0.7000, 0.6000,
+//            0.2000, 0.4000, 0.6000, 0.8000, 1.0000);
 
-    for(int v = 0; v < target.rows; v++)
-    {
-        for (int u = 0; u < target.cols; u++)
-        {
-            U = Vector3f(u, v, 1.0);
-            X = W * U; // get coordinate in source image, X
+    Eigen::Matrix3f W(3,3); // warp function
 
-            float x = X(0);
-            float y = X(1);
-            {
-                float I2 = Interpolate<float>(img_src_f, y, x);
-                target.at<float>(v, u) = I2;
-            }
-        }
-    }
+    //W = Matrix3f::Identity(3, 3);
 
+    W <<    0.7,    -0.7,      0,
+            0.7,      0.7,    0,
+            0,          0,  1;
+
+//    W <<    0,    1,      0,
+//            1,      0,    0,
+//            0,          0,  1;
+//
+    cv::Mat target;
+
+    int out_size[] = {img_src_f.rows, img_src_f.cols};
+    target = ApplyWarp(img_src_f, W, out_size);
+
+    //std::cout << "target Pixels" << std::endl;
+    //std::cout << target << std::endl;
     imshow("original image", img_src_f);
     imshow("warped image", target);
 
     int k = cv::waitKey(0);
-
 }
+

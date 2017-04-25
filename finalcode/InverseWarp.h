7db2eedc04b4e84834b6e71a1a8edb4f0bcb187f
Shun Yao <shunyaorad@gmail.com>
2017-04-25 00:08:14 -0400

    Bug fix for InverseWarp

--- finalcode/InverseWarp.h
+++ finalcode/InverseWarp.h
@@ -6,7 +6,7 @@
 #define INVERSEWARPCOORDS_INVERSEWARP_H_H
 #include <iostream>
 #include <Eigen/Dense>
-#include <opencv2/opencv.hpp>
+#include<opencv2/opencv.hpp>
 #include <cmath>
 
 /**
@@ -15,30 +15,33 @@
  * @input image_size: [rows, cols]
  * @output warpedCoords: [x1, y1, x2, y2, x3, y3, x4, y4]
  */
-void warpCoords(double inCoords[8], int image_size[2], Eigen::Matrix3d warpMat, double &(warpedCoords)[8])
+void warpCoords(double inCoords[8], int image_size[2], Eigen::Matrix3d warpMat, double warpedCoords[8])
 {
     auto imgWidth = image_size[1];
     auto imgHeight = image_size[0];
     double xOffset = (imgWidth + 1)/2.0 - 1;
     double yOffset = (imgHeight + 1)/2.0 - 1;
-    
+
     Eigen::MatrixXd inMatrix(3,4); // [y1, y2, y3, y4; x1, x2, x3, x4; 1, 1, 1, 1]
-    
+
     for (int index = 0; index<4; index++)
     {
         inMatrix.col(index) << inCoords[index * 2 + 1] - yOffset, inCoords[index * 2] - xOffset, 1.0; // (y, x, 1)
     }
-    
+
     warpMat = warpMat.inverse().eval();
     warpMat /= warpMat(2,2);
-    
+
     auto newCoords = warpMat * inMatrix;
-    
-    for (int index = 0; index<3; index++)
+
+    for (int index = 0; index<4; index++)
     {
         warpedCoords[2 * index]     =   newCoords(1,index) / newCoords(2,index) + xOffset; // x
         warpedCoords[2 * index+1]   =   newCoords(0,index) / newCoords(2,index) + yOffset; // y
+        std::cout << warpedCoords[2 * index] << std::endl;
+        std::cout << warpedCoords[2 * index + 1] << std::endl;
     }
+
 }
 
 /**
@@ -53,8 +56,8 @@ void drawBoundingBox(double ptsCoords[8], cv::Mat &img2Draw)
     int linethickness = 1;
     int lineType = 8;
     int shift = 0;
-    
-    for (int lineNum = 0; lineNum < 3; lineNum++)
+
+    for (int lineNum = 0; lineNum < 4; lineNum++)
     {
         int xi  = (int)std::round(ptsCoords[(2 * lineNum)   % 8]);
         int yi  = (int)std::round(ptsCoords[(2 * lineNum+1) % 8]);

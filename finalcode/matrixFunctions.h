e9b93f766d4cef9ba78d63319bb21ac73730e1e5
ashishbajaj91 <mm10b009@smail.iitm.ac.in>
2017-04-27 13:22:51 -0400

    svd not required anymore
    svd not required anymore

--- finalcode/matrixFunctions.h
+++ finalcode/matrixFunctions.h
@@ -3,15 +3,6 @@
 
 #include <Eigen/Dense>
 
-void ComputeSVD(double *s, Eigen::Matrix3d H)
-{
-	Eigen::JacobiSVD<Eigen::Matrix3d> svd(H);
-	auto sing = svd.singularValues();
-	s[0] = sing[0];
-	s[1] = sing[1];
-	s[2] = sing[2];
-}
-
 cv::Mat CheckForNaN(cv::Mat &image)
 {
 	cv::Mat IsNaN = cv::Mat_<double>(image != image)/255;

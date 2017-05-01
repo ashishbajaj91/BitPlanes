4e23e662a6a2a8f91cb0912710a210fe0dd72d2d
ashishbajaj91 <mm10b009@smail.iitm.ac.in>
2017-04-30 20:26:46 -0400

    Fix generate bit planes
    Fix generate bit planes

--- finalcode/generateBitplanes.h
+++ finalcode/generateBitplanes.h
@@ -21,28 +21,16 @@ std::vector<cv::Mat> generateBitPlanes(cv::Mat &grayimage)
 
 	for (int i = 1; i<grayImageWithBorder.rows - 1; i++) 
 	{
-		double* orgp = grayImageWithBorder.ptr<double>(i);
-		double* newp0 = image_channels[0].ptr<double>(i - 1);
-		double* newp1 = image_channels[1].ptr<double>(i - 1);
-		double* newp2 = image_channels[2].ptr<double>(i - 1);
-		double* newp3 = image_channels[3].ptr<double>(i - 1);
-		double* newp4 = image_channels[4].ptr<double>(i - 1);
-		double* newp5 = image_channels[5].ptr<double>(i - 1);
-		double* newp6 = image_channels[6].ptr<double>(i - 1);
-		double* newp7 = image_channels[7].ptr<double>(i - 1);
-
 		for (int j = 1; j<grayImageWithBorder.cols - 1; j++)
 		{
-			double test = orgp[j];
-
-			newp0[j - 1] = (test > (orgp - 1)[j - 1]);
-			newp1[j - 1] = (test > (orgp - 1)[j]);
-			newp2[j - 1] = (test > (orgp - 1)[j + 1]);
-			newp3[j - 1] = (test > orgp[j - 1]);
-			newp4[j - 1] = (test > orgp[j + 1]);
-			newp5[j - 1] = (test > (orgp + 1)[j - 1]);
-			newp6[j - 1] = (test > (orgp + 1)[j]);
-			newp7[j - 1] = (test > (orgp + 1)[j + 1]);
+			image_channels[0].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i-1,j-1);
+			image_channels[1].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i-1,j);
+			image_channels[2].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i-1,j+1);
+			image_channels[3].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i,j-1);
+			image_channels[4].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i,j+1);
+			image_channels[5].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i+1,j-1);
+			image_channels[6].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i+1,j);
+			image_channels[7].at<double>(i-1,j-1) = grayImageWithBorder.at<double>(i,j) > grayImageWithBorder.at<double>(i+1,j+1);
 		}
 	}
 	return image_channels;
